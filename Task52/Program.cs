97c8ce8e81f07c7dc66686b4559ddfe89d337b38
Ayra <777aira777@gmail.com>
2022-10-28 19:59:43 +0400

    Решена 52 задача

--- Task52/Program.cs
+++ Task52/Program.cs
@@ -5,12 +5,12 @@
 // 5 9 2 3
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
-
+ 
 int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
 {
     var matrix = new int[rows, columns];
     var rnd = new Random();
-
+ 
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
@@ -20,7 +20,7 @@ int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
     }
     return matrix;
 }
-
+ 
 void PrintMatrix(int[,] matrix)
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
@@ -34,30 +34,22 @@ void PrintMatrix(int[,] matrix)
         Console.WriteLine("|");
     }
 }
-
-
-
-int[,] Avg(int[,] matrix)
+ 
+void ArithmeticMeanColumn(int[,] arr)
 {
-   double average = default;
-   double sum = default;
-    for (int j = 0; j < matrix.GetLength(1); j++)
+    int sum = 0;
+    Console.Write("Среднее арифметическое каждого столбца : ");
+    for (int j = 0; j < arr.GetLength(1); j++)
     {
-        for (int i = 0; i < matrix.GetLength(0); i++)
+        for (int i = 0; i < arr.GetLength(0); i++)
         {
-          sum = sum + matrix[i,j];
+            sum += arr[i, j];
         }
+        Console.Write(Math.Round((double)sum / arr.GetLength(0), 1) + "  ");
+        sum = 0;
     }
-    return sum;
 }
-
-
-
-int sum = Avg(array2D);
-Console.WriteLine($"{sum}");
-
+ 
 int[,] array2D = CreateMatrixRndInt(3, 4, 1, 10);
 PrintMatrix(array2D);
-
-int sum = Avg(array2D);
-Console.WriteLine($"{sum}");
+ArithmeticMeanColumn(array2D);
